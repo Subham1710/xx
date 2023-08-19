40af3a05572f2c279acc49fc8bd32c98303dd0b3
Bhadkaashiq <subhammishra@MacBook-Air-5.local>
2023-08-19 17:50:09 +0530

    main

---
+++ src/Appbar.jsx
@@ -0,0 +1,104 @@
+// import React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import IconButton from '@mui/material/IconButton';
+// // import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+// const App = () => {
+//   return (
+
+    
+//     <div > 
+//       <AppBar position="static">
+//         <Toolbar>
+//           <div style={{ position: 'absolute', left: 0 }}>
+//             <img
+//               src="/src/logo.png"
+//               alt="Logo"
+//               style={{ width: '100px', height: 'auto' }}
+//             />
+//           </div>
+//           <Typography variant="h4" style={{ flexGrow: 1, textAlign: 'center' }}>
+//             EmptyCup
+//           </Typography>
+//           <IconButton edge="end" color="inherit">
+//             {/* <MoreVertIcon /> */}
+//           </IconButton>
+//         </Toolbar>
+//       </AppBar>
+//       {/* Your other components and content */ }
+
+//       <div style={{ flex: '0 0 25%', padding: '10px', textAlign: 'center' }}>
+//             {/* Icon Component */}
+//             <img src="/src/logo.png" alt="Contact" style={{ width: '50px', height: '50px' }} />
+//             <div>Contact</div>
+//           </div>
+
+//           <div style={{ flex: '0 0 25%', padding: '10px', textAlign: 'center' }}>
+//             {/* Icon Component */}
+//             <img src="/src/logo.png" alt="Contact" style={{ width: '50px', height: '50px' }} />
+//             <div>Gallery</div>
+//           </div>
+
+//           <div style={{ flex: '0 0 25%', padding: '10px', textAlign: 'center' }}>
+//             {/* Icon Component */}
+//             <img src="/src/logo.png" alt="Contact" style={{ width: '50px', height: '50px' }} />
+//             <div>Map</div>
+//           </div>
+
+//           <div style={{ flex: '0 0 25%', padding: '10px', textAlign: 'center' }}>
+//             {/* Icon Component */}
+//             <img src="/src/logo.png" alt="Contact" style={{ width: '50px', height: '50px' }} />
+//             <div>Shortlist </div>
+//           </div>
+
+//           <div style={{ flex: '0 0 25%', padding: '10px', textAlign: 'center' }}>
+//             {/* Icon Component */}
+//             <img src="src/logo.png" alt="Contact" style={{ width: '50px', height: '50px' }} />
+//             <div>Sort </div>
+//           </div>
+
+      
+//       <div>
+      
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import React from 'react';
+
+const DesignComponent = () => {
+  return (
+    <div className="design-container">
+      <div className="empty-cup">@ EmptyCup H</div>
+      <div className="design-row">
+        <span>= =</span>
+        <span>® 1=</span>
+      </div>
+      <div className="epic-designs">Epic Designs </div>
+      <div className="rs2">RS2 8 ¢4</div>
+      <div className="bt">BT e S</div>
+      <div className="numbers">
+        <span>57 8 $$ B</span>
+        <span>+91- 984532853 3</span>
+        <span>+91 - 984532854 -</span>
+      </div>
+      <div className="studio-d3">Studio-D3 s</div>
+      <div className="bottom-row">
+        <span>2 2 2 21</span>
+        <span>e — ®</span>
+        <span>43 8§88 £l</span>
+        <span>+91- 984532853 3</span>
+        <span>+91- 984532854 -</span>
+      </div>
+    </div>
+  );
+};
+
+export default DesignComponent;
+
